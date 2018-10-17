e9a445f770aedb660c86620e4a2f8dd014724efb
keithatan <tan230@purdue.edu>
2018-10-17 01:11:38 -0400

    Created components for different pages

--- keith-personal-website/src/app/app.module.ts
+++ keith-personal-website/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProfileComponent
+  
   ],
   imports: [
     MDBBootstrapModule.forRoot(),
