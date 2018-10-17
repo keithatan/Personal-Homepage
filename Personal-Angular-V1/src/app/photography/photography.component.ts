f11caaaa84617bc325c12d9ead2912d54fab94db
keithatan <tan230@purdue.edu>
2018-10-17 11:55:38 -0400

    Removed Archive and prep for deploy

--- Personal-Angular-V1/src/app/photography/photography.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'photo-cmp',
-    moduleId: module.id,
-    templateUrl: 'photography.component.html'
-})
-
-export class PhotographyComponent{}
--- Personal-Angular-V1/src/app/youtube/youtube.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'yt-cmp',
-    moduleId: module.id,
-    templateUrl: 'youtube.component.html'
-})
-
-export class YoutubeComponent{}
