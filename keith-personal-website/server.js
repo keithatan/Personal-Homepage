6ae71fbead037100852b31638cdacd62bc03d948
keithatan <tan230@purdue.edu>
2018-10-17 12:14:59 -0400

    Change directory path for Heroku

--- keith-personal-website/server.js
+++ keith-personal-website/server.js
@@ -15,11 +15,11 @@ const forceSSL = function() {
   }
 app.use(forceSSL());
 */
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
