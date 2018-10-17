6e2e657db8c27c437cdd9662edb985f5b6a64eca
keithatan <tan230@purdue.edu>
2018-10-17 12:50:12 -0400

    deeeeeployyy

--- keith-personal-website/server.js
+++ keith-personal-website/server.js
@@ -1,24 +1,17 @@
+
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
-/*
-const forceSSL = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  }
-app.use(forceSSL());
-*/
-app.use(express.static(__dirname, 'dist', {index: false}));
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
-  });
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
